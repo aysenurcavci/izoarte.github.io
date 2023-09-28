1877cb327173b6eed4274ffc269d60ba6313d0b2
aysenur <aysenurcavci@gmail.com>
2023-09-29 00:53:50 +0300

    first commit

---
+++ footer.php
@@ -0,0 +1,68 @@
+<!-- Footer Start -->
+<div class="container-fluid bg-dark footer mt-5 py-5 wow fadeIn" data-wow-delay="0.1s">
+    <div class="container py-5">
+        <div class="row g-5 justify-content-between">
+            <div class="col-lg-3 col-md-6">
+                <h5 class="text-white mb-4">Ofisimiz</h5>
+                <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>Hamitler soğukkuyu cad no 40/A Osmangazi BURSA</p>
+                <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>+90 224 249 2555</p>
+                <p class="mb-2"><i class="fa fa-envelope me-3"></i>info@izoarte.com</p>
+                <div class="d-flex pt-3">
+                    <a class="btn btn-square btn-primary rounded-circle me-2" href=""><i class="fab fa-twitter"></i></a>
+                    <a class="btn btn-square btn-primary rounded-circle me-2" href=""><i class="fab fa-facebook-f"></i></a>
+                    <a class="btn btn-square btn-primary rounded-circle me-2" href=""><i class="fab fa-youtube"></i></a>
+                    <a class="btn btn-square btn-primary rounded-circle me-2" href=""><i class="fab fa-linkedin-in"></i></a>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <h5 class="text-white mb-4">Hızlı Linkler</h5>
+                <a class="btn btn-link" href="">Hakkımızda</a>
+                <a class="btn btn-link" href="">İletişim</a>
+                <a class="btn btn-link" href="">Projeler</a>
+                <a class="btn btn-link" href="">Faaliyetlerimiz</a>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <h5 class="text-white mb-4">Çalışma Saatleri</h5>
+                <p class="mb-1">Pazartesi - Cumartesi</p>
+                <h6 class="text-light">08:00 - 18:00 </h6>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Footer End -->
+
+
+<!-- Copyright Start -->
+<div class="container-fluid copyright bg-dark py-4">
+    <div class="container text-center">
+        <span>Copyright &copy;
+            <script>
+                document.write(new Date().getFullYear())
+            </script>,Tüm Hakları Saklıdır
+        </span>
+
+        <p class="mb-0">Designed &amp; Developed by <a class="fw-semi-bold">Ayşe Nur ÇAVCI
+        </p>
+    </div>
+</div>
+<!-- Copyright End -->
+
+
+<!-- Back to Top -->
+<a href="#" class="btn btn-lg btn-primary btn-lg-square rounded-circle back-to-top"><i class="bi bi-arrow-up"></i></a>
+
+
+<!-- JavaScript Libraries -->
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+<script src="lib/wow/wow.min.js"></script>
+<script src="lib/easing/easing.min.js"></script>
+<script src="lib/waypoints/waypoints.min.js"></script>
+<script src="lib/owlcarousel/owl.carousel.min.js"></script>
+<script src="lib/counterup/counterup.min.js"></script>
+
+<!-- Template Javascript -->
+<script src="js/main.js"></script>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,342 @@
+<?php
+$sayfa = 'anasayfa'; // Bu sayfa aktifse
+include 'header.php';
+?>
+
+<!-- Carousel Start -->
+<div class="container-fluid px-0 mb-5">
+    <div id="header-carousel" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img class="w-100" src="img/carousel-1.jpg" alt="Image">
+                <div class="carousel-caption">
+                    <div class="container">
+                        <div class="row justify-content-center">
+                            <div class="col-lg-10 text-start">
+                                <p class="fs-5 fw-medium text-primary text-uppercase animated slideInRight">25 Yıllık Tecrübe</p>
+                                <h1 class="display-1 text-white mb-5 animated slideInRight">Anahtar Teslim <br> Endüstriyel Yapılar</h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img class="w-100" src="img/carousel-2.jpg" alt="Image">
+                <div class="carousel-caption">
+                    <div class="container">
+                        <div class="row justify-content-center">
+                            <div class="col-lg-10 text-start">
+                                <p class="fs-5 fw-medium text-primary text-uppercase animated slideInRight">Su Isı Ses Yalıtımı</p>
+                                <h1 class="display-1 text-white mb-5 animated slideInRight">Yalıtım İşlerinizde Güçlü Bir Çözüm Ortağı
+
+                                </h1>
+                                <a href="" class="btn btn-primary py-3 px-5 animated slideInRight">Daha Fazla</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#header-carousel" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Önceki</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#header-carousel" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Sonraki</span>
+        </button>
+    </div>
+</div>
+<!-- Carousel End -->
+
+
+<!-- About Start -->
+<div class="container-xxl py-5">
+    <div class="container">
+        <div class="row g-5">
+            <div class="col-lg-6">
+                <div class="row gx-3 h-100">
+                    <div class="col-6 align-self-start wow fadeInUp" data-wow-delay="0.1s">
+                        <img class="img-fluid" src="img/about-1.jpg">
+                    </div>
+                    <div class="col-6 align-self-end wow fadeInDown" data-wow-delay="0.1s">
+                        <img class="img-fluid" src="img/about-2.jpg">
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6 wow fadeIn" data-wow-delay="0.5s">
+                <p class="fw-medium text-uppercase text-primary mb-2">HAKKIMIZDA</p>
+                <h1 class="display-5 mb-4">Endüstriyel Pazarda Lideriz</h1>
+                <p class="mb-4">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu diam amet diam et
+                    eos. Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo justo magna dolore erat amet
+                </p>
+                <div class="d-flex align-items-center mb-4">
+                    <div class="flex-shrink-0 bg-primary p-4 center ">
+                        <h1 class="display-2" style="color: #FFFFFF">25</h1>
+                        <h5 class="text-white">Yıllık</h5>
+                        <h5 class="text-white">Tecrübe</h5>
+                    </div>
+                    <div class="ms-4">
+                        <p><i class="fa fa-check text-primary me-2"></i>Deneyimli Kadro</p>
+                        <p><i class="fa fa-check text-primary me-2"></i>Ekonomik</p>
+                        <p><i class="fa fa-check text-primary me-2"></i>Güvenilir</p>
+                        <p><i class="fa fa-check text-primary me-2"></i>Estetik</p>
+                    </div>
+                </div>
+                <div class="row pt-2">
+                    <div class="col-sm-6">
+                        <div class="d-flex align-items-center">
+                            <div class="flex-shrink-0 btn-lg-square rounded-circle bg-primary">
+                                <i class="fa fa-envelope-open text-white"></i>
+                            </div>
+                            <div class="ms-3">
+                                <p class="mb-2">Email</p>
+                                <h5 class="mb-0">info@izoarte.com</h5>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="d-flex align-items-center">
+                            <div class="flex-shrink-0 btn-lg-square rounded-circle bg-primary">
+                                <i class="fa fa-phone-alt text-white"></i>
+                            </div>
+                            <div class="ms-3">
+                                <p class="mb-2">Bize Ulaşın</p>
+                                <h5 class="mb-0">+90 224 249 2555</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- About End -->
+
+<!-- Features Start -->
+<div class="container-xxl py-5">
+    <div class="container">
+        <div class="row g-5 align-items-center">
+            <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s">
+                <div class="position-relative me-lg-4">
+                    <img class="img-fluid w-100" src="img/feature.jpg" alt="">
+                    <span></span>
+                    </button>
+                </div>
+            </div>
+            <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.5s">
+                <h1 class="display-5 mb-4">Neden İZOARTE?</h1>
+                <p class="mb-4">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu diam amet diam et
+                    eos. Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo justo magna dolore erat amet
+                </p>
+                <div class="row gy-4">
+                    <div class="col-12">
+                        <div class="d-flex">
+                            <div class="flex-shrink-0 btn-lg-square rounded-circle bg-primary">
+                                <i class="fa fa-check text-white"></i>
+                            </div>
+                            <div class="ms-4">
+                                <h4>Experienced Workers</h4>
+                                <span>Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo justo magna
+                                    dolore erat amet</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12">
+                        <div class="d-flex">
+                            <div class="flex-shrink-0 btn-lg-square rounded-circle bg-primary">
+                                <i class="fa fa-check text-white"></i>
+                            </div>
+                            <div class="ms-4">
+                                <h4>Reliable Industrial Services</h4>
+                                <span>Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo justo magna
+                                    dolore erat amet</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12">
+                        <div class="d-flex">
+                            <div class="flex-shrink-0 btn-lg-square rounded-circle bg-primary">
+                                <i class="fa fa-check text-white"></i>
+                            </div>
+                            <div class="ms-4">
+                                <h4>24/7 Customer Support</h4>
+                                <span>Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo justo magna
+                                    dolore erat amet</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Features End -->
+
+<!-- Service Start -->
+<div class="container-xxl py-5">
+    <div class="container">
+        <div class="text-center mx-auto pb-4 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 600px;">
+            <p class="fw-medium text-uppercase text-primary mb-2">Faaliyetlerimiz</p>
+            <h1 class="display-5 mb-4">En İyi Endüstriyel Hizmetleri Sunuyoruz</h1>
+        </div>
+        <div class="row gy-5 gx-4">
+            <div class="col-md-6 col-lg-4 wow fadeInUp" data-wow-delay="0.1s">
+                <div class="service-item">
+                    <img class="img-fluid" src="img/service-1.jpg" alt="">
+                    <div class="service-img">
+                        <img class="img-fluid" src="img/service-1.jpg" alt="">
+                    </div>
+                    <div class="service-detail">
+                        <div class="service-title">
+                            <hr class="w-25">
+                            <h3 class="mb-0">Endüstriyel Yapılar</h3>
+                            <hr class="w-25">
+                        </div>
+                        <div class="service-text">
+                            <p class="text-white mb-0">Erat ipsum justo amet duo et elitr dolor, est duo duo eos
+                                lorem sed diam stet diam sed stet.</p>
+                        </div>
+                    </div>
+                    <a class="btn btn-light" href="">Read More</a>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-4 wow fadeInUp" data-wow-delay="0.3s">
+                <div class="service-item">
+                    <img class="img-fluid" src="img/service-2.jpg" alt="">
+                    <div class="service-img">
+                        <img class="img-fluid" src="img/service-2.jpg" alt="">
+                    </div>
+                    <div class="service-detail">
+                        <div class="service-title">
+                            <hr class="w-25">
+                            <h3 class="mb-0">Çelik Konstruksiyon</h3>
+                            <hr class="w-25">
+                        </div>
+                        <div class="service-text">
+                            <p class="text-white mb-0">Erat ipsum justo amet duo et elitr dolor, est duo duo eos
+                                lorem sed diam stet diam sed stet.</p>
+                        </div>
+                    </div>
+                    <a class="btn btn-light" href="">Read More</a>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-4 wow fadeInUp" data-wow-delay="0.5s">
+                <div class="service-item">
+                    <img class="img-fluid" src="img/service-3.jpg" alt="">
+                    <div class="service-img">
+                        <img class="img-fluid" src="img/service-3.jpg" alt="">
+                    </div>
+                    <div class="service-detail">
+                        <div class="service-title">
+                            <hr class="w-25">
+                            <h3 class="mb-0">Dekorasyon/Tadilat</h3>
+                            <hr class="w-25">
+                        </div>
+                        <div class="service-text">
+                            <p class="text-white mb-0">Erat ipsum justo amet duo et elitr dolor, est duo duo eos
+                                lorem sed diam stet diam sed stet.</p>
+                        </div>
+                    </div>
+                    <a class="btn btn-light" href="">Read More</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Service End -->
+
+
+<!-- Project Start -->
+<div class="container-fluid bg-dark pt-5 my-5 px-0">
+    <div class="text-center mx-auto mt-5 wow fadeIn" data-wow-delay="0.1s" style="max-width: 600px;">
+        <p class="fw-medium text-uppercase text-primary mb-2">Projelerimiz</p>
+        <h1 class="display-5 text-white mb-5">Yakın Zamanda Neleri Tamamladık</h1>
+    </div>
+    <div class="owl-carousel project-carousel wow fadeIn" data-wow-delay="0.1s">
+        <a class="project-item" href="">
+            <img class="img-fluid" src="img/project-1.jpg" alt="">
+            <div class="project-title">
+                <h5 class="text-primary mb-0">Auto Engineering</h5>
+            </div>
+        </a>
+        <a class="project-item" href="">
+            <img class="img-fluid" src="img/project-2.jpg" alt="">
+            <div class="project-title">
+                <h5 class="text-primary mb-0">Civil Engineering</h5>
+            </div>
+        </a>
+        <a class="project-item" href="">
+            <img class="img-fluid" src="img/project-3.jpg" alt="">
+            <div class="project-title">
+                <h5 class="text-primary mb-0">Gas Engineering</h5>
+            </div>
+        </a>
+        <a class="project-item" href="">
+            <img class="img-fluid" src="img/project-4.jpg" alt="">
+            <div class="project-title">
+                <h5 class="text-primary mb-0">Power Engineering</h5>
+            </div>
+        </a>
+        <a class="project-item" href="">
+            <img class="img-fluid" src="img/project-5.jpg" alt="">
+            <div class="project-title">
+                <h5 class="text-primary mb-0">Energy Engineering</h5>
+            </div>
+        </a>
+        <a class="project-item" href="">
+            <img class="img-fluid" src="img/project-6.jpg" alt="">
+            <div class="project-title">
+                <h5 class="text-primary mb-0">Water Engineering</h5>
+            </div>
+        </a>
+    </div>
+</div>
+<!-- Project End -->
+
+
+
+<!-- Testimonial Start -->
+<div class="container-xxl py-5">
+    <div class="container">
+        <div class="text-center mx-auto wow fadeInUp" data-wow-delay="0.1s" style="max-width: 200px;">
+            <h1 class="display-5 mb-5">Referanslar</h1>
+        </div>
+        <div class="owl-carousel testimonial-carousel wow fadeInUp" data-wow-delay="0.1s">
+            <div class="testimonial-item text-center">
+                <div class="testimonial-img position-relative">
+                    <img class="img-fluid rounded-circle mx-auto mb-5" src="img/testimonial-1.jpg">
+
+                </div>
+                <div class="testimonial-text text-center rounded p-4">
+                    <h5 class="mb-1">Client Name</h5>
+                    <span class="fst-italic">Profession</span>
+                </div>
+            </div>
+            <div class="testimonial-item text-center">
+                <div class="testimonial-img position-relative">
+                    <img class="img-fluid rounded-circle mx-auto mb-5" src="img/testimonial-2.jpg">
+
+                </div>
+                <div class="testimonial-text text-center rounded p-4">
+                    <h5 class="mb-1">Client Name</h5>
+                    <span class="fst-italic">Profession</span>
+                </div>
+            </div>
+            <div class="testimonial-item text-center">
+                <div class="testimonial-img position-relative">
+                    <img class="img-fluid rounded-circle mx-auto mb-5" src="img/testimonial-3.jpg">
+
+                </div>
+                <div class="testimonial-text text-center rounded p-4">
+                    <h5 class="mb-1">Client Name</h5>
+                    <span class="fst-italic">Profession</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Testimonial End -->
+<?php
+include 'footer.php';
+?>
